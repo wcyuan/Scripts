3e4c3e9177d1bc9314cbc58e40d96fda09bfc46c
Conan Yuan <conanyuan@gmail.com>
2012-01-20 11:39:49 -0500

    add perldoc

--- tools/rmdups.pl
+++ tools/rmdups.pl
@@ -1,10 +1,30 @@
 #!/usr/local/bin/perl -w
 #
-# Given a line, return the line with duplicates removed.
-# Also, remove certain specified elements.
-#
-# Can be useful for fixing up a PATH
-#
+
+=head1 SYNOPSIS
+
+  rmdups.pl [-FS <separator>] [-exclude <string>] <string>*
+
+=head1 DESCRIPTION
+
+Given a list of strings, return the list with duplicates elements
+removed.  The order of elements will be the order in which they first
+appear in the original list.
+
+To get the list of strings, takes its arguments, join the arguments
+with space (" "), then split them again with $FS, which defaults to
+space, but can be specified with the -F or -FS options.
+
+If there are no arguments, read from stdin and assume that each line
+is a list.  Split the line by FS to get the list.
+
+If you pass in elements to exclude with -exclude, this will filter out
+those elements.
+
+Can be useful for fixing up a PATH
+
+=cut
+
 use strict;
 use Getopt::Long;
 
