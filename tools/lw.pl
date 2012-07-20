fdcbc01c96ca7520eb12bf29fec7e3c4a3a1ea04
Conan Yuan <conanyuan@gmail.com>
2012-07-20 16:53:09 -0400

    lw - find perl and python modules as well

--- tools/lw.pl
+++ tools/lw.pl
@@ -15,6 +15,31 @@ my ($n) = abs($opt_n) if defined($opt_n);
 my @files = ();
 my $less_in = 0;
 my $to_less;
+
+sub find_perl_module($) {
+    my ($file) = @_;
+    my $cmd = "desperldoc -v $file 3>/dev/null 2>&1 1>&3 | grep '^Found as'";
+    chomp(my $perldoc_output = `$cmd`);
+    if ($? == 0 && $perldoc_output !~ m/^\s*$/) {
+        my $perl_module = (split ' ', $perldoc_output, 3)[2];
+        if (!defined($perl_module) || $perl_module =~ m/^\s*$/) {
+        } else {
+            return $perl_module;
+        }
+    }
+    return $file;
+}
+
+sub find_python_module($) {
+    my ($file) = @_;
+    my $cmd = "despydoc --where $file 2>/dev/null";
+    chomp(my $python_module = `$cmd`);
+    if ($? == 0) {
+        return $python_module;
+    }
+    return $file;
+}
+
 foreach my $cmd (@ARGV) {
     # If the command has a / in it, then it's probably the full path
     # already, no need to use where to find it.
@@ -22,9 +47,11 @@ foreach my $cmd (@ARGV) {
         push(@files, $cmd);
         next;
     }
+    my $found = 0;
     open(WHERE,"echo where $cmd | tcsh -s |") or warn "$cmd: $! $?";
     while(<WHERE>) {
 	next if (defined($n) && $INPUT_LINE_NUMBER != $n);
+        $found = 1;
 	chomp;
 	if (-x $_) {
 	    push(@files, $_);
@@ -39,7 +66,22 @@ foreach my $cmd (@ARGV) {
 	}
     }
     close(WHERE);
+
+    if (!$found) {
+        if ($cmd =~ /::/) {
+            my $pm = find_perl_module($cmd);
+            if (defined($pm)) {
+                push(@files, $pm);
+            }
+        } else {
+            my $py = find_python_module($cmd);
+            if (defined($py)) {
+                push(@files, $py);
+            }
+        }
+    }
 }
+
 if (scalar(@files) > 0) {
     my $cmd = "less " . join(" ", @files);
     if ($less_in) {
