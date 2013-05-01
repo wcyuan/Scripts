8926decfeaa6c94d0ebc3ceb9cecf8d59ed290a7
Conan Yuan <conanyuan@gmail.com>
2013-05-01 15:42:11 -0400

    separate filtering from transposing move more of the processing into read_files add ability to join rows from different columns

--- tools/hgrep.pl
+++ tools/hgrep.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings 'all';
 use Getopt::Long qw(:config pass_through);
 use Log::Log4perl qw(:levels);
+use List::MoreUtils qw(pairwise all firstidx);
 
 # -----------------------------------------------
 
@@ -14,26 +15,43 @@ my $LOGGER = Log::Log4perl->get_logger();
 # -----------------------------------------------
 
 sub main() {
-    my ($cols, $files, $strs) = get_options();
+    my ($cols, $files, $strs, $join_on) = get_options();
     if (scalar(@$strs) == 0) {
         return;
     }
-    my $lines = read_files($files, $strs);
-    pretty(transpose($lines, $cols));
+    my $table;
+    foreach my $file (@$files) {
+        my $lines = read_files([$file], $strs);
+        if (!defined($table)) {
+            $table = $lines;
+        } else {
+            $table = join_tables($table, $lines, $join_on);
+        }
+    }
+    print pretty(filter(transpose($table), $cols));
 }
 
 # -----------------------------------------------
 
-sub default_file() {
-    chomp(my $date = `date +%Y%m%d`);
-    my $fn = "/data/optvol/eomm/universe/PROD/$date/underlying_universe.txt";
-    $LOGGER->debug("Reading $fn");
-    return $fn;
+sub default_files() {
+    chomp(my $sdate = `date +%Y/%m/%d`);
+    (my $date = $sdate) =~ s@/@@g;
+    my $univ = "/data/optvol/eomm/universe/PROD/$date/underlying_universe.txt";
+    $LOGGER->debug("Reading $univ");
+    my $quotes = `ls -t /data/optvol/eomm/feed_dumps/PROD/$sdate/stock_quotes.* | head -1`;
+    $LOGGER->debug("Reading $quotes");
+    return ($univ, $quotes);
+}
+
+sub default_cols() {
+    return qw(ticker spn desname bid ask shares_outstanding volume);
 }
 
 sub get_options() {
 
-    GetOptions('verbose|v' => sub { $LOGGER->level($DEBUG) });
+    GetOptions('verbose|v' => sub { $LOGGER->level($DEBUG) },
+               'join|on=s'    => \my @join_on,
+              );
 
     # Consume all options.  They just become the columns that we
     # should print.
@@ -52,10 +70,16 @@ sub get_options() {
         push(@files, pop(@ARGV));
     }
     if (scalar(@files) == 0) {
-        push(@files, default_file());
+        @files = default_files();
+        if (scalar(@cols) == 0) {
+            @cols = default_cols();
+        }
+        if (scalar(@join_on) == 0) {
+            @join_on = ('spn', 'ticker');
+        }
     }
 
-    return (\@cols, \@files, \@ARGV);
+    return (\@cols, \@files, \@ARGV, \@join_on);
 }
 
 # -----------------------------------------------
@@ -68,50 +92,120 @@ sub read_files($$) {
     # TODO: handle user specified patterns
     # TODO: handle compressed files
     my $patt = join('|', map {"^$_ "} ($MAGIC, @$strs));
-    my @lines;
+    my @alllines;
     foreach my $file (@$files) {
         my $cmd = "egrep '$patt' $file";
         $LOGGER->debug("Running $cmd");
-        my $lines = `$cmd`;
+        my $text = `$cmd`;
 
-        # Split on spaces, but allow backslash to escape spaces
-        my @flines = map { [split('(?<!\\\) ', $_)] } split('\n', $lines);
-        if (scalar(@$files) > 1) {
-            if (scalar(@flines) > 0) {
-                push(@{$flines[0]}, 'FILE');
-                for (my $ii = 1; $ii < scalar(@flines); $ii++) {
-                    push(@{$flines[$ii]}, $file);
+        my @lines = split('\n', $text);
+        my @flines;
+        for (my $ii = 0; $ii < scalar(@lines); $ii++) {
+            # Split on spaces, but allow backslash to escape spaces.
+            my @flds = split('(?<!\\\) ', $lines[$ii]);
+
+            # Remove backslash in front of spaces, those were
+            # only for escaping
+            foreach (@flds) { s/\\ / /g };
+
+            # If this is the header, discard the $MAGIC keyword
+            if (scalar(@flds) > 0 && $flds[0] eq $MAGIC) {
+                shift(@flds);
+            }
+
+            # If we have multiple files, add the filename
+            if (0 && scalar(@$files) > 1) {
+                if ($ii == 0) {
+                    unshift(@flds, 'FILE');
+                } else {
+                    unshift(@flds, $file);
                 }
             }
-            push(@lines, @flines);
-        } else {
-            @lines = @flines;
+            push(@flines, \@flds);
+        }
+
+        push(@alllines, @flines);
+    }
+    return \@alllines;
+}
+
+# -----------------------------------------------
+
+# return the index of the first value in @$list that matches $val;
+sub findidx($$) {
+    my ($list, $val) = @_;
+    return firstidx {$_ eq $val} @$list;
+}
+
+# convert a list of values into their corresponding indexes in a given
+# list
+sub indexes($$) {
+    my ($list, $vals) = @_;
+    return map {findidx($list, $_)} @$vals;
+}
+
+# given a list of indexs, return the corresponding values in @$list
+sub take($$) {
+    my ($list, $idxs) = @_;
+    return map { $list->[$_] } @$idxs;
+}
+
+# The opposite of take
+sub skip($$) {
+    my ($list, $idxs) = @_;
+    my @new;
+    for (my $ii = 0; $ii < scalar(@$list); $ii++) {
+        if (!contains($idxs, $ii)) {
+            push(@new, $list->[$ii]);
         }
     }
-    return \@lines;
+    return @new;
+}
+
+sub arr_equal($$) {
+    my ($arr1, $arr2) = @_;
+    our ($a, $b);
+    return all { $_ } pairwise { $a eq $b }  @$arr1, @$arr2;
+}
+
+sub join_tables($$$) {
+    my ($table1, $table2, $on) = @_;
+    # If nothing to join on, then just concatenate the tables
+    if (!defined($on) || scalar(@$on) < 1 ||
+        scalar(@$table1) < 1 || scalar(@$table2) < 1)
+    {
+        return [ @$table1, @$table2 ];
+    }
+
+    my $header1 = shift(@$table1);
+    my $idxs1 = [indexes($header1, $on)];
+    my $header2 = shift(@$table2);
+    my $idxs2 = [indexes($header2, $on)];
+    my @newtable = [@$header1, skip($header2, $idxs2)];
+    foreach my $row1 (@$table1) {
+        foreach my $row2 (@$table2) {
+            if (arr_equal([take($row1, $idxs1)], [take($row2, $idxs2)])) {
+                push(@newtable, [@$row1, skip($row2, $idxs2)]);
+            }
+        }
+    }
+
+
+    return \@newtable;
 }
 
 # -----------------------------------------------
 
 # $intable should be a list of lists
-#
-# Besides transposing, this also handles the filtering of the column.
-# Ideally that would be a separate function.
 
-sub transpose($$) {
-    my ($intable, $cols) = @_;
+sub transpose($) {
+    my ($intable) = @_;
     my @outtable;
 
     while (1) {
         my $cont = 0;
         my $doprint = 1;
         my @output;
-        if (@$cols > 0) {
-            unless (scalar(@$intable) > 0 && scalar(@{$intable->[0]}) > 0 &&
-                    scalar(grep { $_ eq $intable->[0][0] } @$cols) > 0) {
-                $doprint = 0;
-            }
-        }
 
         for (my $ii = 0; $ii < @$intable; $ii++) {
             my $line = $intable->[$ii];
@@ -119,24 +213,41 @@ sub transpose($$) {
             if (scalar(@$line) > 0) {
                 $cont = 1;
                 $val = shift(@$line);
-                if ($val eq $MAGIC) {
-                    $val = shift(@$line);
-                }
-                $val =~ s/\\ / /g;
             }
 
-            # if we are extracting exactly one column, then don't
-            # print the column name.
-            unless ($doprint && @$cols == 1 && $ii == 0) {
-                push(@output, $val);
-            }
+            push(@output, $val);
         }
-        push(@outtable, \@output)
-            if $doprint;
+        push(@outtable, \@output);
         last unless $cont;
     }
 
     return \@outtable
+}
+
+# -----------------------------------------------
+
+# return true iff $val is in @$list.
+sub contains($$) {
+    my ($list, $val) = @_;
+    return scalar(grep { $_ eq $val } @$list) > 0;
+}
+
+# Filter rows whose first column matches one of the specified values
+sub filter($$) {
+    my ($table, $cols) = @_;
+    if (@$cols > 0) {
+        @$table = grep { contains($cols, $_->[0]) } @$table;
+
+        # if we are extracting exactly one column, then don't
+        # print the column name.
+        if (scalar(@$cols) == 1) {
+            foreach my $row (@$table) {
+                shift(@$row)
+                    if scalar(@$row) > 0;
+            }
+        }
+    }
+    return $table;
 }
 
 # -----------------------------------------------
@@ -153,12 +264,14 @@ sub pretty($) {
             }
         }
     }
+    my $output;
     foreach my $line (@$outtable) {
         for (my $ii = 0; $ii < scalar(@$line); $ii++) {
-            printf("%-$widths[$ii]s ", $line->[$ii]);
+            $output .= sprintf("%-$widths[$ii]s ", $line->[$ii]);
         }
-        print "\n";
+        $output .= "\n";
     }
+    return $output;
 }
 
 # -----------------------------------------------
