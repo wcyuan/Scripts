d5e29610c487f93b86a284f7f6c0eecebd5a8d6d
Conan Yuan <conanyuan@gmail.com>
2013-06-21 14:48:15 -0400

    re-organize code:
    clean up the Job class (move around methods, add comments)
    
    move the logic for translating the Job object into a separate DbJob object
    
    output to the terminal in addition to saving stdout and stderr
    
    separate the code to running the job, save output and return codes, from the code to handle errors

--- tools/longjob.py
+++ tools/longjob.py
@@ -20,8 +20,11 @@ import logging
 import optparse
 import os
 import os.path
+import select
 import subprocess
+import sys
 import time
+import traceback
 
 from email.mime.text import MIMEText
 
@@ -47,22 +50,25 @@ def main():
             for job in table.get_recent_jobs(7):
                 print job.summary()
     else:
-        result = Job.new_job(args, table=table).run()
-        Mailer(subject=result.summary(), body=result.report()).send()
+        Job.new_job(args, table=table, shell=opts.shell).run()
 
 # --------------------------------------------------------------------
 
 def getopts():
     parser = optparse.OptionParser()
     parser.add_option('--verbose',
-                      help='turn on debug logging',
-                      action='store_true')
+                      action='store_true',
+                      help='turn on debug logging')
     parser.add_option('--no_write',
-                      help='turn off all side effects',
-                      action='store_true')
+                      action='store_true',
+                      help='turn off all side effects')
     parser.add_option('-d', '--config_dir', '--dir',
                       help='location of the config directory',
                       default=DEFAULT_CONFIG_DIR)
+    parser.add_option('-s', '--shell',
+                      action='store_true',
+                      help='use the shell to parse the command')
+
     opts, args = parser.parse_args()
 
     if opts.verbose:
@@ -153,137 +159,131 @@ class Job(object):
     FAILED    = 'failed'
     SUCCEEDED = 'succeeded'
 
-    _status_ids = (NEVER_RUN, RUNNING, FAILED, SUCCEEDED)
-
     TIME_FORMAT = '%Y%m%d %H:%M:%S'
 
+    # -----------------------
+    # Constructors
+
     def __init__(self,
-                 cmd             = None,
-                 strcmd          = None,
                  table           = None,
-                 status          = None,
-                 status_id       = None,
+                 job_id          = None,
+                 cmd             = None,
+                 status          = NEVER_RUN,
+                 start_time      = None,
+                 end_time        = None,
                  rc              = -1,
                  stdout          = '',
                  stderr          = '',
-                 start_time      = None,
-                 start_time_secs = None,
-                 end_time        = None,
-                 end_time_secs   = None,
-                 job_id          = None):
-
-        if cmd is None:
-            if strcmd is None:
-                raise ValueError("No command provided")
-            else:
-                cmd = strcmd
+                 shell           = False):
+        """
+        This initialize could be for an existing job or a new job that
+        has never been run.
+        """
+        self.table      = table
+        self.job_id     = job_id
         if isinstance(cmd, basestring):
             self.cmd    = cmd.split()
             self.strcmd = cmd
         else:
             self.cmd    = cmd
             self.strcmd = ' '.join(cmd)
-        self.status     = (status if status is not None
-                           else self.status_id_to_name(status_id)
-                           if status_id is not None else self.NEVER_RUN)
+        self.status     = status
+        self.start_time = start_time
+        self.end_time   = end_time
         self.rc         = rc
         self.stdout     = stdout
         self.stderr     = stderr
-        self.start_time = (start_time if start_time is not None
-                           else time.localtime(start_time_secs)
-                           if start_time_secs is not None
-                           else None)
-        self.end_time   = (end_time if end_time is not None
-                           else time.localtime(end_time_secs)
-                           if end_time_secs is not None
-                           else None)
-        self.table      = table
-        self.job_id     = job_id
+        self.shell      = shell
 
     @classmethod
-    def new_job(cls, cmd, table=None):
-        obj = cls(cmd=cmd, table=table)
+    def new_job(cls, cmd, table=None, shell=False):
+        """
+        This is a constructor for a new job that has never been run.
+        """
+        obj = cls(cmd=cmd, table=table, shell=shell)
         obj.addtodb()
         return obj
 
+    # -----------------------
+    # Running jobs
+
     def run(self):
-        if self.NO_WRITE:
-            logging.info("NO WRITE: {0}".format(self.strcmd))
-            process = None
-        else:
-            logging.info("Running:  {0}".format(self.strcmd))
-            process = subprocess.Popen(self.cmd,
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE)
         self.start_time = time.localtime()
         self.status = self.RUNNING
+        self.updatedb()
 
         # If we get a control-c, mark it as a failure
         import signal
-        def handle():
-            logging.info("Received SIGTERM, marking job as a failure")
-            self.end_time = time.localtime()
+        def handle(signum, frame):
+            logging.error("Received SIGTERM (signum = {0}), marking "
+                          "job as a failure".format(signum))
             self.status = self.FAILED
-            self.updatedb()
+            self.stderr += 'Received SIGTERM'
+            self._finish()
         signal.signal(signal.SIGTERM, handle)
 
-        self.updatedb()
+        try:
+            if self.NO_WRITE:
+                logging.info("NO WRITE: {0}".format(self.strcmd))
+                # in NO_WRITE mode, pretend we succeeded
+                self.rc = 0
+            else:
+                self._run_capture_output()
+        except:
+            logging.error("Received exception, marking job as a failure")
+            self.stderr += traceback.format_exc()
+            raise
+        finally:
+            if self.rc == 0:
+                self.status = self.SUCCEEDED
+            else:
+                self.status = self.FAILED
+            self._finish()
 
-        if self.NO_WRITE:
-            # in NO_WRITE mode, pretend we succeeded
-            self.status = self.SUCCEEDED
-            self.rc = 0
-            self.stdout = ''
-            self.stderr = ''
-        else:
-            try:
-                (self.stdout, self.stderr) = process.communicate()
-                self.rc = process.returncode
-            except:
-                logging.info("Received exception, marking job as a failure")
-                self.rc = 1
-
-        self.end_time = time.localtime()
-        if self.rc == 0:
-            self.status = self.SUCCEEDED
-        else:
-            self.status = self.FAILED
-        self.updatedb()
-        logging.info("Finished")
         return self
 
-    @property
-    def status_id(self):
-        return self.status_name_to_id(self.status)
+    def _finish(self):
+        self.end_time = time.localtime()
+        self.updatedb()
+        Mailer(subject=self.summary(),
+               body=self.report()).send()
+        logging.info("Finished")
 
-    @classmethod
-    def status_name_to_id(cls, status_name):
-        return cls._status_ids.index(status_name)
 
-    @classmethod
-    def status_id_to_name(cls, status_id):
-        return cls._status_ids[status_id]
+    def _run_capture_output(self):
+        if self.shell:
+            logging.info("Running (shell): {0}".format(self.strcmd))
+            proc = subprocess.Popen(self.strcmd,
+                                    shell=self.shell,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
+        else:
+            logging.info("Running: {0}".format(self.strcmd))
+            proc = subprocess.Popen(self.cmd,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
 
-    @property
-    def finished(self):
-        return self.status in (self.FAILED, self.SUCCEEDED)
+        # We capture stdout and stderr, but we also output them to the
+        # terminal.
+        # http://stackoverflow.com/questions/12270645/can-you-make-a-python-subprocess-output-stdout-and-stderr-as-usual-but-also-cap
+        while True:
+            ret = select.select([proc.stdout.fileno(),
+                                 proc.stderr.fileno()], [], [])
+            for fd in ret[0]:
+                if fd == proc.stdout.fileno():
+                    read = proc.stdout.readline()
+                    sys.stdout.write(read)
+                    self.stdout += read
+                if fd == proc.stderr.fileno():
+                    read = proc.stderr.readline()
+                    sys.stderr.write(read)
+                    self.stderr += read
+            if proc.poll() != None:
+                break
+        self.rc = proc.returncode
 
-    @property
-    def duration(self):
-        secs = self.end_time_secs - self.start_time_secs
-        if secs < 0:
-            return 'never finished'
-        output = []
-        for (num, name) in ((7 * 24 * 60 * 60, 'week'),
-                            (    24 * 60 * 60, 'day'),
-                            (         60 * 60, 'hr'),
-                            (              60, 'min')):
-            if secs > num:
-                output.append('{0} {1}'.format(int(secs / num),
-                                               name))
-                secs = secs % num
-        output.append('{0} {1}'.format(int(secs), 'sec'))
-        return ' '.join(output)
+    # -----------------------
+    # Time formatting
 
     @classmethod
     def _format_time(cls, time_struct):
@@ -317,14 +317,37 @@ class Job(object):
     def end_time_secs(self):
         return self._time_secs(self.end_time)
 
+    @property
+    def duration(self):
+        secs = self.end_time_secs - self.start_time_secs
+        if secs < 0:
+            return 'never finished'
+        output = []
+        for (num, name) in ((7 * 24 * 60 * 60, 'week'),
+                            (    24 * 60 * 60, 'day'),
+                            (         60 * 60, 'hr'),
+                            (              60, 'min')):
+            if secs > num:
+                output.append('{0} {1}'.format(int(secs / num),
+                                               name))
+                secs = secs % num
+        output.append('{0} {1}'.format(int(secs), 'sec'))
+        return ' '.join(output)
+
+    # -----------------------
+    # DB updates
+
     def updatedb(self):
         if self.table is not None:
-            self.table.update_job(self)
+            self.table.update_job(DbJob(self))
 
     def addtodb(self):
         if self.table is not None:
-            self.job_id = self.table.add_job(self)
+            self.job_id = self.table.add_job(DbJob(self))
             logging.info("Got job id {0}".format(self.job_id))
+
+    # -----------------------
+    # Text summaries
 
     def summary(self):
         return ('Job id {self.job_id:3} {self.status:10} '
@@ -468,7 +491,7 @@ class SqlTable(object):
             return
         self.db.execute("CREATE TABLE jobs ( "
                         "job_id INTEGER PRIMARY KEY, {0});".
-                        format(', '.join('{0} {1}'.format(ci[0], ci[2])
+                        format(', '.join('{0} {1}'.format(ci[0], ci[1])
                                          for ci in self.columns)))
 
     def add_obj(self, obj):
@@ -477,7 +500,7 @@ class SqlTable(object):
                         format(self.table,
                                ', '.join(ci[0] for ci in self.columns),
                                ', '.join('?' for ci in self.columns)),
-                        [getattr(obj, ci[1]) for ci in self.columns])
+                        [getattr(obj, ci[0]) for ci in self.columns])
         return self.db.lastrowid
 
     def update_obj(self, obj):
@@ -486,7 +509,7 @@ class SqlTable(object):
                                ', '.join('{0} = ?'.format(ci[0])
                                          for ci in self.columns),
                                self.key_col),
-                        [getattr(obj, ci[1]) for ci in self.columns] +
+                        [getattr(obj, ci[0]) for ci in self.columns] +
                         [getattr(obj, self.key_attr)])
 
     def get_obj(self, key):
@@ -511,7 +534,7 @@ class SqlTable(object):
         return [self._row_to_obj(r) for r in self.db.fetchall()]
 
     def _row_to_obj(self, row):
-        kwargs = dict((col_info[1], val)
+        kwargs = dict((col_info[0], val)
                       for (val, col_info) in zip(row, self.columns))
         kwargs[self.key_attr] = row[-1]
         logging.debug(kwargs)
@@ -519,18 +542,57 @@ class SqlTable(object):
 
 # --------------------------------------------------------------------
 
+class DbJob(object):
+    """
+    A class for translating between a Job object and a row in our
+    JobTable
+    """
+
+    STATUS_IDS = (Job.NEVER_RUN, Job.RUNNING, Job.FAILED, Job.SUCCEEDED)
+
+    def __init__(self, job):
+        self.job_id     = job.job_id
+        self.command    = job.strcmd
+        self.status     = self.STATUS_IDS.index(job.status)
+        self.start_time = job.start_time_secs
+        self.end_time   = job.end_time_secs
+        self.returncode = job.rc
+        self.stdout     = job.stdout
+        self.stderr     = job.stderr
+        self.shell      = bool(job.shell)
+
+    @classmethod
+    def make_job(cls, job_id, command, status, start_time, end_time,
+                 returncode, stdout, stderr, shell):
+        return Job(job_id     = job_id,
+                   cmd        = command.split(),
+                   status     = cls.STATUS_IDS[status],
+                   start_time = time.localtime(start_time),
+                   end_time   = time.localtime(end_time),
+                   rc         = returncode,
+                   stdout     = stdout,
+                   stderr     = stderr,
+                   shell      = bool(shell))
+
+    @classmethod
+    def status_name_to_id(cls, *statuses):
+        return [cls.STATUS_IDS.index(s) for s in statuses]
+
+# --------------------------------------------------------------------
+
 class JobTable(object):
     def __init__(self, filename):
         self.table = SqlTable(
             filename,
-            obj_constructor=Job,
-            columns=(('command',    'strcmd',          'TEXT'),
-                     ('status',     'status_id',       'INTEGER'),
-                     ('start_time', 'start_time_secs', 'INTEGER'),
-                     ('end_time',   'end_time_secs',   'INTEGER'),
-                     ('returncode', 'rc',              'INTEGER'),
-                     ('stdout',     'stdout',          'TEXT'),
-                     ('stderr',     'stderr',          'TEXT'),
+            obj_constructor=DbJob.make_job,
+            columns=(('command',    'TEXT'),
+                     ('status',     'INTEGER'),
+                     ('start_time', 'INTEGER'),
+                     ('end_time',   'INTEGER'),
+                     ('returncode', 'INTEGER'),
+                     ('stdout',     'TEXT'),
+                     ('stderr',     'TEXT'),
+                     ('shell',      'INTEGER'),
                      ),
             table='jobs',
             key_col='job_id',
@@ -561,20 +623,18 @@ class JobTable(object):
 
     def get_running_jobs(self):
         return self.table.get_objs('WHERE status IN (?)',
-                                   [Job.status_name_to_id(s)
-                                    for s in (Job.RUNNING,)])
+                                   DbJob.status_name_to_id(Job.RUNNING))
 
     def get_incomplete_jobs(self, since=None):
         return self.table.get_objs('WHERE status NOT IN (?, ?) {0}'.
                                    format(self.get_since(since)),
-                                   [Job.status_name_to_id(s)
-                                    for s in (Job.SUCCEEDED, Job.RUNNING)])
+                                   DbJob.status_name_to_id(Job.SUCCEEDED,
+                                                           Job.RUNNING))
 
     def get_succeeded_jobs(self, since=None):
         return self.table.get_objs('WHERE status IN (?) {0}'.
                                    format(self.get_since(since)),
-                                   [Job.status_name_to_id(s)
-                                    for s in (Job.SUCCEEDED,)])
+                                   DbJob.status_name_to_id(Job.SUCCEEDED))
 
     def get_recent_jobs(self, since=None):
         return (self.get_running_jobs() +
