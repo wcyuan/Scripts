dc08b471aeb41f0f5f68cd9f7998eaa210250fbd
Conan Yuan <conanyuan@gmail.com>
2013-06-20 23:24:50 -0400

    first version of fleshed out sqlite code

--- tools/longjob.py
+++ tools/longjob.py
@@ -128,21 +128,52 @@ class Job(object):
     FAILED    = 'failed'
     SUCCEEDED = 'succeeded'
 
+    _status_ids = (NEVER_RUN, RUNNING, FAILED, SUCCEEDED)
+
     TIME_FORMAT = '%Y%m%d %H:%M:%S'
 
-    def __init__(self, cmd):
+    def __init__(self,
+                 cmd             = None,
+                 table           = None,
+                 status          = None,
+                 status_id       = None,
+                 rc              = None,
+                 stdout          = None,
+                 stderr          = None,
+                 start_time      = None,
+                 start_time_secs = None,
+                 end_time        = None,
+                 end_time_secs   = None,
+                 job_id          = None):
+
         if isinstance(cmd, basestring):
             self.cmd    = cmd.split()
             self.strcmd = cmd
         else:
             self.cmd    = cmd
             self.strcmd = ' '.join(cmd)
-        self.status = self.NEVER_RUN
-        self.rc     = None
-        self.stdout = None
-        self.stderr = None
-        self.start_time = None
-        self.end_time   = None
+        self.status     = (status if status is not None
+                           else self.status_id_to_string(status_id)
+                           if status_id is not None else self.NEVER_RUN)
+        self.rc         = rc
+        self.stdout     = stdout
+        self.stderr     = stderr
+        self.start_time = (start_time if start_time is not None
+                           else time.localtime(start_time_secs)
+                           if start_time_secs is not None
+                           else None)
+        self.end_time   = (end_time if end_time is not None
+                           else time.localtime(end_time_secs)
+                           if end_time_secs is not None
+                           else None)
+        self.table      = table
+        self.job_id     = job_id
+
+    @classmethod
+    def new_job(cls, cmd, table=None):
+        obj = cls(cmd=cmd, table=table)
+        obj.addtodb()
+        return obj
 
     def run(self):
         if self.NO_WRITE:
@@ -156,6 +187,7 @@ class Job(object):
 
         self.start_time = time.localtime()
         self.status = self.RUNNING
+        self.updatedb()
         (self.stdout, self.stderr) = process.communicate()
         self.end_time = time.localtime()
         self.rc = process.returncode
@@ -163,8 +195,17 @@ class Job(object):
             self.status = self.SUCCEEDED
         else:
             self.status = self.FAILED
+        self.updatedb()
         logging.info("Finished")
         return self
+
+    @property
+    def status_id(self):
+        return self._status_ids.index(self.status)
+
+    @classmethod
+    def status_id_to_string(cls, status_id):
+        return cls._status_ids[status_id]
 
     @property
     def finished(self):
@@ -174,7 +215,7 @@ class Job(object):
     def duration(self):
         if not self.finished:
             return 'never_run'
-        secs = time.mktime(self.end_time) - time.mktime(self.start_time)
+        secs = self.end_time_secs - self.start_time_secs
         output = []
         for (num, name) in ((24 * 60 * 60, 'day'),
                             (60 * 60,      'hr'),
@@ -199,7 +240,25 @@ class Job(object):
     def end_time_str(self):
         return self._format_time(self.end_time)
 
+    @property
+    def start_time_secs(self):
+        return time.mktime(self.start_time)
+
+    @property
+    def end_time_secs(self):
+        return time.mktime(self.end_time)
+
+    def updatedb(self):
+        if self.table is not None:
+            self.table.update_row(self)
+
+    def addtodb(self):
+        if self.table is not None:
+            self.job_id = self.table.add_row(self)
+
     def summary(self):
+        # todo: make this more informative.  Include start/end times
+        # and a portion of the command being run
         return 'Job {0}'.format(self.status)
 
     def report(self):
@@ -218,19 +277,93 @@ class Job(object):
 
 # --------------------------------------------------------------------
 
-class JobTable(object):
+class SqlDb(object):
     def __init__(self, filename):
         import sqlite3
+        self.filename = filename
+        self.conn = sqlite3.connect(filename)
+        self.cursor = self.conn.cursor()
 
+    def __repr__(self):
+        return ('{cn}({fn!r})'.format(cn=type(self).__name__,
+                                      fn=self.filename))
+
+    def execute_always(self, *args, **kwargs):
+        """
+        Run a command without checking NO_WRITE.  Use this for
+        non-state changing commands, like queries.  Or use this for
+        state changing commands after you have already checked
+        NO_WRITE, but in that case you need to call commit yourself.
+        """
         # http://docs.python.org/2/library/sqlite3.html
         # # This is the qmark style:
         # cur.execute("insert into people values (?, ?)", (who, age))
         #
         # # And this is the named style:
         # cur.execute("select * from people where name_last=:who and age=:age", {"who": who, "age": age})
+        logging.info("SqlDb Executing: '{0}' '{1}'".format(args, kwargs))
+        return self.cursor.execute(*args, **kwargs)
 
-        self.conn = sqlite3.connect(filename)
-        self.cursor = self.conn.cursor()
+    def execute(self, *args, **kwargs):
+        """
+        Run state changing commands, if allowed by NO_WRITE
+        """
+        if Job.NO_WRITE:
+            logging.info("SqlDb NO WRITE: would run '{0}' '{1}'".
+                         format(args, kwargs))
+        else:
+            self.execute_always(*args, **kwargs)
+            self.commit()
+
+    def commit(self):
+        self.conn.commit()
+
+    def fetchall(self):
+        return self.cursor.fetchall()
+
+    def query_one_row_always(self, *args, **kwargs):
+        self.execute_always(*args, **kwargs)
+        return self.fetch_one_row()
+
+    def fetch_one_row(self):
+        rows = self.cursor.fetchall()
+        if len(rows) != 1:
+            raise ValueError("Too many matching rows")
+        return rows[0]
+
+    def fetch_one_value(self):
+        return self.fetch_one_row()[0]
+
+    def query_one_value_always(self, *args, **kwargs):
+        self.query_one_row_always(*args, **kwargs)[0]
+
+    @property
+    def lastrowid(self):
+        return self.cursor.lastrowid
+
+# --------------------------------------------------------------------
+
+class SqlTable(object):
+    def __init__(self, filename, obj_constructor, columns, table, key_col):
+        """
+        @param obj_constructor: this is a function which takes each of
+        the object_attributes as keyword arguments and returns an
+        instance of that object.
+
+        @param columns: a mapping from the table's columns to the
+        object's attributes and the table's sql types.  Should be a
+        list of lists having this form:
+          [
+            [ table_column_name1, object_attribute_name1, sql_type1 ],
+            [ table_column_name2, object_attribute_name2, sql_type2 ],
+            [ table_column_name3, object_attribute_name3, sql_type3 ]
+          ]
+        """
+        self.db = SqlDb(filename)
+        self.obj_ctor  = obj_constructor
+        self.columns   = columns
+        self.table     = table
+        self.key_col   = key_col
         self._create_table_if_necessary()
 
     def _create_table_if_necessary(self):
@@ -248,21 +381,72 @@ class JobTable(object):
         # cursor = con.cursor()
         # cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         # print(cursor.fetchall())
-        pass
+        self.db.execute("SELECT name "
+                        "FROM   sqlite_master "
+                        "WHERE  type = 'table' "
+                        "  AND  name = '{0}';".format(self.table))
+        matches = self.db.fetchall()
+        if len(matches) > 0:
+            return
+        self.db.execute("CREATE TABLE jobs ( "
+                        "job_id INTEGER PRIMARY KEY, {0});".
+                        format(', '.join('{0} {1}'.format(ci[0], ci[2])
+                                         for ci in self.columns)))
 
-    def add_job(self, job):
+    def add_row(self, obj):
         # http://stackoverflow.com/questions/6242756/how-to-retrieve-inserted-id-after-inserting-row-in-sqlite-using-python
-        self.cursor.execute("INSERT INTO {0} () VALUES (?, ?)", ())
-        return self.cursor.lastrowid
+        self.db.execute("INSERT INTO {0} ({1}) VALUES ({2})".
+                        format(self.table,
+                               ', '.join(ci[0] for ci in self.columns),
+                               ', '.join('?' for ci in self.columns)),
+                        [getattr(obj, ci[1]) for ci in self.columns])
+        return self.db.lastrowid
 
-    def update_job(self, job):
-        pass
+    def update_obj(self, obj):
+        self.db.execute("UPDATE {0} ({1}) VALUES ({2}) WHERE {3} = ?".
+                        format(self.table,
+                               ', '.join(ci[0] for ci in self.columns),
+                               ', '.join('?' for ci in self.columns),
+                               self.key_col),
+                        [getattr(obj, ci[1]) for ci in self.columns] +
+                        [getattr(obj, self.key_col)])
 
-    def get_job(self, job):
-        pass
+    def get_obj(self, key):
+        return self.row_to_obj(self.db.query_one_row_always(
+                "SELECT {0} FROM {1} WHERE {2} = {3}".
+                format(', '.join(ci[0] for ci in self.columns),
+                       self.table, self.key_col, key)))
 
-    def get_all_jobs(self):
-        pass
+    def get_objs(self, where):
+        self.db.execute_always("SELECT * FROM {0} {1}".
+                               format(self.table, where))
+        return [self.row_to_obj(r) for r in self.db.fetchall()]
+
+    def row_to_obj(self, row):
+        kwargs = dict(col_info[1], val
+                      for (val, col_info) in zip(row, self.columns))
+        return self.obj_ctor(**kwargs)
+
+# --------------------------------------------------------------------
+
+class JobTable(SqlTable):
+    # Not clear if this should be a subclass if it should just have a
+    # SqlTable.  Maybe the latter, then we'll replace methods like
+    # get_obj with get_job.
+    def __init__(self, filename):
+        super(JobTable, self).__init__(
+            filename,
+            obj_class=Job,
+            columns=(('command',    'strcmd',     'TEXT'),
+                     ('status',     'status_id',  'INTEGER'),
+                     ('start_time', 'start_time', 'INTEGER'),
+                     ('end_time',   'end_time',   'INTEGER'),
+                     ('returncode', 'rc',         'INTEGER'),
+                     ('stdout',     'stdout',     'TEXT'),
+                     ('stderr',     'stderr',     'TEXT'),
+                     ),
+            table='jobs',
+            key_col='job_id')
 
 # --------------------------------------------------------------------
 
