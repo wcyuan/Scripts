17b8076917309b9c1e1fab12bcb36ad684473cbf
Conan Yuan <conanyuan@gmail.com>
2013-07-30 10:09:42 -0400

    add el-get

--- dotfiles/dot.xemacs/init.el
+++ dotfiles/dot.xemacs/init.el
@@ -977,3 +977,21 @@ Enters shell-script[tcsh] mode (see `shell-script-mode')."
 ;;     (fontify))
 
 ;; ---------------------------------------------------------------- ;;
+;; el-get
+;;
+;; https://github.com/dimitri/el-get
+;;
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+;; ---------------------------------------------------------------- ;;
