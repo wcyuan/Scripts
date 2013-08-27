c87b684facf84161406a5c7988fd8ea9b65da1f1
Conan Yuan <conanyuan@gmail.com>
2013-08-27 10:15:32 -0400

    add tail

--- tools/head.py
+++ tools/head.py
@@ -12,48 +12,7 @@ import itertools
 
 def indices(seq, *args):
     """
-    Print a part of a sequence
-
-    # No args, defaults to first 20 elements
-    >>> head(range(100))
-    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
-
-    # One arg is like [:A]
-    >>> head(range(100), 10)
-    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-
-    # Two args, is like [A:B]
-    >>> head(range(100), 3, 10)
-    (3, 4, 5, 6, 7, 8, 9)
-
-    # Three args, is like [A:B:C]
-    >>> head(range(100), 3, 10, 2)
-    (3, 5, 7, 9)
-
-    # One negative args, is like [-A:]
-    >>> head(range(100), -3)
-    (97, 98, 99)
-
-    # Two args, if any are negative, is like [-A:-B]
-    >>> head(range(100), -10, -5)
-    (90, 91, 92, 93, 94)
-
-    # works for generators, too
-    >>> head(i for i in xrange(100))
-    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
-    >>> head((i for i in xrange(100)), 10)
-    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-    >>> head((i for i in xrange(100)), 3, 10)
-    (3, 4, 5, 6, 7, 8, 9)
-    >>> head((i for i in xrange(100)), 3, 10, 2)
-    (3, 5, 7, 9)
-
-    # For generators, we don't know the length before hand, so the
-    # negative args don't really work
-    >>> head((i for i in xrange(100)), -3)
-    (0, 1, 2)
-    >>> head((i for i in xrange(100)), -10, -5)
-    (0, 1, 2, 3, 4)
+    Get the indices needed for head
     """
     #
     # Parse arguments as, roughly:
@@ -90,8 +49,63 @@ def indices(seq, *args):
 
 
 def head(seq, *args, **kwargs):
+    """
+    Print a part of a sequence
+
+    # No args, defaults to first 20 elements
+    >>> head(xrange(100))
+    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
+
+    # One arg is like [:A]
+    >>> head(xrange(100), 10)
+    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+    # Two args, is like [A:B]
+    >>> head(xrange(100), 3, 10)
+    (3, 4, 5, 6, 7, 8, 9)
+
+    # Three args, is like [A:B:C]
+    >>> head(xrange(100), 3, 10, 2)
+    (3, 5, 7, 9)
+
+    # One negative args, is like [-A:]
+    >>> head(xrange(100), -3)
+    (97, 98, 99)
+
+    # Two args, if any are negative, is like [-A:-B]
+    >>> head(xrange(100), -10, -5)
+    (90, 91, 92, 93, 94)
+
+    # works for generators, too
+    >>> head(i for i in xrange(100))
+    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
+    >>> head((i for i in xrange(100)), 10)
+    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    >>> head((i for i in xrange(100)), 3, 10)
+    (3, 4, 5, 6, 7, 8, 9)
+    >>> head((i for i in xrange(100)), 3, 10, 2)
+    (3, 5, 7, 9)
+
+    # For generators, we don't know the length before hand, so the
+    # negative args don't really work
+    >>> head((i for i in xrange(100)), -3)
+    (0, 1, 2)
+    >>> head((i for i in xrange(100)), -10, -5)
+    (0, 1, 2, 3, 4)
+    """
     (start, end, step) = indices(seq, *args)
     return tuple(itertools.islice(seq, start, end, step, **kwargs))
+
+def tail(seq, num=20):
+    """
+    >>> tail(xrange(100))
+    (80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99)
+    >>> tail(xrange(100), 5)
+    (95, 96, 97, 98, 99)
+    >>> tail(xrange(5))
+    (0, 1, 2, 3, 4)
+    """
+    return head(seq, -num)
 
 # --------------------------------------------------------------------
 
