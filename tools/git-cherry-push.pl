5cf307c4f9a869dbbcc93e25ffdb0a22ce467a5e
Conan Yuan <conanyuan@gmail.com>
2014-01-24 00:06:19 -0500

    get rid of the idea of pushing to a release branch.
    clean up perldoc
    
    allow the revision to be a filename
    
    better error handling -- if there was an error, don't run git pull on
    the original branch

--- tools/git-cherry-push.pl
+++ tools/git-cherry-push.pl
@@ -10,8 +10,6 @@ git-cherry-push.pl - cherry-pick a single local commit and push it
   git-cherry-push.pl [options] <rev>
 
   Options:
-    --release         push it to the release tree
-    --both            push it to both the master and release trees
     --help, -?        shows brief help message
     --perldoc         shows full documentation
 
@@ -23,7 +21,7 @@ git-cherry-push.pl - cherry-pick a single local commit and push it
 
 =item I<rev>
 
-The revision to push
+The revision to push (optional, defaults to the HEAD revision)
 
 =back
 
@@ -31,13 +29,14 @@ The revision to push
 
 =over 4
 
-=item I<--release>
+=item I<--upstream [remote/branch]>
 
-Push it to the release tree.  This assumes we are in a GUAS repo.
+Specify the upstream branch to push to
 
-=item I<--both>
+=item I<--leave>
 
-Push it to both the master and release trees
+If there is an error, don't clean up, leave it in the conflicted state
+so the user can investigate.
 
 =item I<--help>
 
@@ -51,12 +50,10 @@ Print the perldoc page and exit.
 
 =head1 DESCRIPTION
 
-For master (assumes we want to push to remote/origin)
-
     o We assume that we are in a git repo
-    o create a new branch tracking remote/origin
+    o create a new temp branch tracking the upstream of the current branch
     o git stash (if necessary)
-    o switch to that branch
+    o switch to the temp branch
     o git cherry-pick the revision
     o git pull
     o git push
@@ -64,15 +61,6 @@ For master (assumes we want to push to remote/origin)
     o git pull
     o git stash pop (if necessary)
     o delete temp branch
-
-For release (assumes a particular repository):
-
-    o cd to release tree
-    o git pull
-    o git cherry-pick the revision
-    o git push
-    o cd to shared repository
-    o git pull
 
 =cut
 
@@ -84,74 +72,40 @@ use Carp;
 # --------------------------------------------
 
 my $DEBUG = 0;
-my $SHARED_RELEASE = '/testbed/github/release';
 
 sub main() {
-    my ($rev, $master, $release, $release_tree, $upstream, $leave) =
-        parse_command_line();
+    my ($rev, $upstream, $leave) = parse_command_line();
 
-    if ($master) {
-        push_to_master($rev, $upstream, $leave);
-    }
-
-    if ($release) {
-        push_to_release($rev, $release_tree);
-    }
+    pick_and_push($rev, $upstream, $leave);
 }
 
 # --------------------------------------------
 
 sub parse_command_line() {
-    my $release_tree = $SHARED_RELEASE;
-
-    my $user = getpwuid($>) || getpwuid($<) || getlogin();
-    if ($user eq 'yuanc') {
-        $release_tree = '/home/Yuan/testbed/github/release';
-    }
-
     # Parse any command-line options
-    GetOptions('master!'          => \my $master,
-               'release!'         => \my $release,
-               'both!'            => \my $both,
-               'upstream=s'       => \my $upstream,
+    GetOptions('upstream=s'       => \my $upstream,
                'leave!'           => \my $leave,
                'debug|no_write!'  => \$DEBUG)
         or die("Bad arguments");
-
-    #
-    # no options means just do the master branch
-    # -master    means just do the master branch
-    # -release   means just do the release branch
-    # -both      means do both branches
-    #
-
-    if ($both) {
-        $master = 1;
-        $release = 1;
-    } else {
-        if (!defined($master)) {
-            if ($release) {
-                $master = 0;
-            } else {
-                $master = 1;
-            }
-        }
-    }
 
     pod2usage("Wrong number of arguments") unless @ARGV <= 1;
 
     my ($rev) = @ARGV;
 
     if (!defined($rev)) {
-        $rev = run('git log --pretty=format:%H -n 1',
-                   {always => 1, return_output => 1});
+        $rev = '';
     }
+    # $rev can be anything that git log understands, including a
+    # filename (in which case we'll push the last committed change to
+    # that file)
+    $rev = run("git log --pretty=format:\%H -n 1 $rev",
+               {always => 1, return_output => 1});
 
     my $msg = run("git log -n 1 $rev",
                   {always => 1, return_output => 1});
     print "Pushing rev $msg\n";
 
-    return ($rev, $master, $release, $release_tree, $upstream, $leave);
+    return ($rev, $upstream, $leave);
 }
 
 sub run($;$) {
@@ -198,7 +152,7 @@ sub upstream() {
                {always => 1, return_output => 1});
 }
 
-sub push_to_master($;$$) {
+sub pick_and_push($;$$) {
     my ($rev, $upstream, $leave) = @_;
 
     my $orig = current_branch();
@@ -217,6 +171,7 @@ sub push_to_master($;$$) {
         print("\nError running cherry-pick, aborting.  To be left in the " .
               "middle of the conflict, use the --leave option.\n");
         run('git reset --merge');
+        run("git checkout $orig");
     } else {
         run('git pull');
         my $rc = run("git push", {no_die_on_error => !$leave});
@@ -224,37 +179,16 @@ sub push_to_master($;$$) {
             print("\nError running push, aborting.  To be left in the " .
                   "middle of the conflict, use the --leave option.\n");
             run('git reset --merge');
+            run("git checkout $orig");
+        } else {
+            run("git checkout $orig");
+            run('git pull');
         }
     }
-    run("git checkout $orig");
-    run('git pull');
     if ($stashed) {
         run('git stash pop');
     }
     run('git branch -D temp');
-}
-
-sub cd($) {
-    my ($dir) = @_;
-
-    print("cd $dir\n");
-    chdir($dir)
-        or confess("Can't cd to $dir");
-}
-
-sub push_to_release($$) {
-    my ($rev, $release_tree) = @_;
-
-    cd($release_tree);
-
-    run('git pull');
-    run("git cherry-pick $rev");
-    run("git push");
-
-    if ($release_tree ne $SHARED_RELEASE) {
-        cd($SHARED_RELEASE);
-        run('git pull');
-    }
 }
 
 # --------------------------------------------
