06e8dff05139229d2f13e9a1c82011d83cf4020e
Conan Yuan <conanyuan@gmail.com>
2014-03-05 10:36:08 -0500

    Add template.py

---
+++ tools/template.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+"""
+
+# --------------------------------------------------------------------------- #
+
+from __future__ import absolute_import, division, with_statement
+
+import optparse
+import logging
+
+logging.basicConfig(format='[%(asctime)s '
+                    '%(funcName)s:%(lineno)s %(levelname)-5s] '
+                    '%(message)s')
+
+# --------------------------------------------------------------------------- #
+
+def main():
+    (opts, args) = getopts()
+
+def getopts():
+    parser = optparse.OptionParser()
+    parser.add_option('--verbose',  action='store_true')
+    (opts, args) = parser.parse_args()
+    if opts.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+    return (opts, args)
+
+# --------------------------------------------------------------------------- #
+
+if __name__ == "__main__":
+    main()
+
+# --------------------------------------------------------------------------- #
