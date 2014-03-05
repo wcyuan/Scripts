eabdad51836744a18d302031c95f679a8db20cd6
Conan Yuan <conanyuan@gmail.com>
2014-03-05 11:34:51 -0500

    add a bunch of options
    1. Option to output a diff instead of all the new output
    2. No write and verbose modes, to see what it would do without changing the saved state
    3. show_blanks mode, to output something non-blank if the new output is blank

--- tools/output_if_changed.py
+++ tools/output_if_changed.py
@@ -6,9 +6,14 @@ time we are called, if the stdin hasn't changed, then don't output it.
 
 from __future__ import absolute_import, division, with_statement
 
+import logging
 import optparse
 import os
 import sys
+
+logging.basicConfig(format='[%(asctime)s '
+                    '%(funcName)s:%(lineno)s %(levelname)-5s] '
+                    '%(message)s')
 
 DEFAULT_STATE = '/tmp/output_if_changed.out'
 
@@ -24,22 +29,48 @@ def get_data(filename):
     with open(filename) as fd:
         return fd.read()
 
+def splitlines(data, eol="\n"):
+    """
+    Split a string into lines, but keep the newline character.
+    """
+    if data is None:
+        return ()
+    return tuple(d + eol for d in data.split(eol))
+
+def udiff(before, after):
+    import difflib
+    return difflib.unified_diff(splitlines(before), splitlines(after),
+                                fromfile='old-output', tofile='new-output')
+
 def main():
     """
     Main body of the script.
     """
-    (_, statefile) = getopts()
+    (opts, statefile) = getopts()
     old_data = get_data(statefile)
     new_data = sys.stdin.read()
     if old_data != new_data:
-        sys.stdout.write(new_data)
-        write_data(statefile, new_data)
+        if opts.diffs:
+            for line in udiff(old_data, new_data):
+                sys.stdout.write(line)
+        elif opts.show_blanks and new_data == '':
+            print "<new output is empty>"
+        else:
+            sys.stdout.write(new_data)
+        if opts.no_write:
+            logging.info("NO WRITE: Not storing changes to %s", statefile)
+        else:
+            write_data(statefile, new_data)
 
 def getopts():
     """
     Parse the command-line options
     """
     parser = optparse.OptionParser()
+    parser.add_option('--diffs', action='store_true')
+    parser.add_option('--show_blanks', action='store_true')
+    parser.add_option('--no_write', action='store_true')
+    parser.add_option('--verbose', action='store_true')
     opts, args = parser.parse_args()
     if not args:
         statefile = DEFAULT_STATE
@@ -48,6 +79,9 @@ def getopts():
     else:
         raise ValueError("Too many arguments: {0}".
                          format(args))
+
+    if opts.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
 
     return opts, statefile
 
