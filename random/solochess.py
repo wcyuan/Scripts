1c00a11e449310105b0e94f89da418b987dabcff
Conan Yuan <conanyuan@gmail.com>
2014-10-14 11:44:11 -0400

    When you generate a puzzle, you've generated its solution too
    Also:
    
    Improve the documentation and comments
    
    Remove unused from_string method
    
    Remove --generate option.  As long as no pieces are given, we assume
    generate is true.
    
    Make change board.move method to take a reverse option instead of
    force, so we can be clearer about the assertions in the reverse case.

--- random/solochess.py
+++ random/solochess.py
@@ -32,7 +32,6 @@ P B . .
 
 # ------------------------------------------------------------- #
 
-import itertools
 import logging
 import optparse
 import sys
@@ -46,21 +45,28 @@ PIECE_SEP = ','
 
 def main():
     opts = getopts()
+
+    # Make the board.  If no pieces are given, generate a new random board
     board = Board(rows=opts.rows, cols=opts.cols, pieces_str=opts.pieces)
+    if opts.pieces is None:
+        (board, moves) = generate_random_puzzle(board, npieces=opts.npieces)
 
-    if opts.generate:
-        board = generate_random_puzzle(board, npieces=opts.npieces)
-
+    # Print the board and make a copy for later when we are showing
+    # the solution
     print board
-
-    if opts.generate:
-        raw_input()
-
     boardcopy = board.copy()
-    moves = do_search(board)
-    print board
+
+    # If we generated the board randomly, we have the solution
+    # already, otherwise look for a solution.
+    if opts.pieces is None:
+        raw_input()
+    else:
+        moves = do_search(board)[1]
+
+    # Print the solution
+    moves = reversed(moves)
     print moves
-    for (piece, loc, end) in reversed(moves[1]):
+    for (piece, loc, end) in moves:
         boardcopy.capture(piece, loc, end)
         print boardcopy
 
@@ -71,7 +77,6 @@ def getopts():
     parser.add_option('--cols', type=int, default=DEFAULT_SIZE)
     parser.add_option('-n', type=int)
     parser.add_option('--verbose', action='store_true')
-    parser.add_option('--generate', action='store_true')
     parser.add_option('--npieces', type=int, default=DEFAULT_N_PIECES)
     opts, args = parser.parse_args()
     if len(args) != 0:
@@ -81,7 +86,6 @@ def getopts():
     if opts.n is not None:
         opts.rows = opts.n
         opts.cols = opts.n
-        opts.nqueens = opts.n
     return opts
 
 # ------------------------------------------------------------- #
@@ -143,11 +147,29 @@ class Piece(object):
         return char == cls.CHAR
 
     @classmethod
+    def valid(cls, board, locs):
+        return [loc for loc in locs if board.is_on_board(loc)]
+
+    @classmethod
+    def empty(cls, board, locs):
+        return [loc for loc in locs if board.is_empty(loc)]
+
+    @classmethod
+    def valid_empty(cls, board, locs):
+        return cls.empty(board, cls.valid(board, locs))
+
+    @classmethod
     def moves(cls, board, loc, white=True):
         """
         Returns a list of all the locations that this piece is allowed to
         move.  This is the same as attacks, except for pawns which
         move differently when capturing.
+
+        @param white: boolean which is true iff we are moving a white
+        piece.  White's side is row 0, (so White pawn can only move in
+        increasing row numbers) while Black's side is the highest row
+        and its pawn can only move down.
+
         """
         return cls.attacks(board, loc, white=white)
 
@@ -158,19 +180,18 @@ class Piece(object):
         move, including capturing.  This is the same as moves, except
         for pawns which move differently when capturing.
         """
-        return [loc for loc in cls.raw_attacks(board, loc, white=white)
-                if board.is_on_board(loc)]
-
+        return cls.valid(board, cls.raw_attacks(board, loc, white=white))
 
     @classmethod
     def moved_from(cls, board, loc, white=True):
         """
         Returns a list of all the locations that this piece may have moved from.
 
-        Defaults to just the list of moves, since for most pieces,
-        moves are symmetric.
+        Defaults to the list of moves from empty cells, since for most
+        pieces, moves are symmetric.
+
         """
-        return cls.moves(board, loc, white=white)
+        return cls.empty(board, cls.moves(board, loc, white=white))
 
     @classmethod
     def attacked_from(cls, board, loc, white=True):
@@ -178,10 +199,10 @@ class Piece(object):
         Returns a list of all the locations that this piece may have
         attacked from
 
-        Defaults to just the list of attacks, since for most pieces,
-        attacks are symmetric.
+        Defaults to the same as moved_from, since for most pieces, you
+        attack the same way you move.
         """
-        return cls.attacks(board, loc, white=white)
+        return cls.moved_from(board, loc, white=white)
 
     @classmethod
     def raw_attacks(cls, board, loc, white=True):
@@ -215,10 +236,6 @@ class Pawn(Piece):
     CHAR = 'P'
 
     @classmethod
-    def valid(cls, board, locs):
-        return [loc for loc in locs if board.is_on_board(loc)]
-
-    @classmethod
     def moves(cls, board, loc, white=True):
         """
         This isn't quite right.  This returns the places where the piece
@@ -238,7 +255,7 @@ class Pawn(Piece):
         """
         (row, col) = loc
         new_row = row + (-1 if white else 1)
-        return cls.valid(board, [(new_row, col)])
+        return cls.valid_empty(board, [(new_row, col)])
 
     @classmethod
     def raw_attacks(cls, board, loc, white=True):
@@ -266,7 +283,7 @@ class Pawn(Piece):
         """
         (row, col) = loc
         new_row = row + (-1 if white else 1)
-        return cls.valid(board, [(new_row, col+i) for i in (1, -1)])
+        return cls.valid_empty(board, [(new_row, col+i) for i in (1, -1)])
 
 class King(Piece):
     CHAR = 'K'
@@ -338,12 +355,13 @@ class Board(FieldMixin):
     def __init__(self, rows=DEFAULT_SIZE, cols=DEFAULT_SIZE,
                  pieces=None, pieces_str=None):
         """
-        A board is represented as a list of rows x cols values.  Each
-        value can either be empty, or it can contain a queen, or it
-        can be attacked by a queen.
+        A board is represented as a list of rows x cols values.  A spot on
+        the board is usually referred to by a "location" or "loc",
+        which is a (row, col) tuple.  Row and col are zero-indexed
+        integers (e.g., zero is the first row).  Row zero is the
+        bottom and Column zero is the left-most.
 
-        A spot on the board is usually referred to by a "location" or
-        "loc", which is a (row, col) tuple.
+        @param pieces: a list of tuples (piece, (row, col))
 
         """
         super(Board, self).__init__()
@@ -403,6 +421,9 @@ class Board(FieldMixin):
         return eval(repr(self))
 
     def get(self, loc):
+        """
+        Get the value at a location
+        """
         idx = self._loc_to_idx(loc)
         try:
             return self.board[idx]
@@ -412,6 +433,11 @@ class Board(FieldMixin):
             raise tp, value, traceback
 
     def _set(self, loc, value):
+        """
+        Set the value at a location.  This should not be used much,
+        usually you want to use add_piece since that will update
+        self.pieces.
+        """
         idx = self._loc_to_idx(loc)
         try:
             self.board[idx] = value
@@ -423,8 +449,7 @@ class Board(FieldMixin):
 
     def _reset_board(self):
         """
-        Call this after changing self.queens to recompute which locations
-        are being attacked.
+        Initializes the board and the pieces.
         """
         self.board = [self.EMPTY] * (self.rows * self.cols)
         for (piece, loc) in self.pieces:
@@ -432,14 +457,25 @@ class Board(FieldMixin):
         return self
 
     def is_empty(self, loc):
+        """
+        @return true if the given location of the board is empty
+        """
         return self.get(loc) == self.EMPTY
 
     @classmethod
     def valid_pieces(cls):
+        """
+        The list of all the types of pieces that we know about.
+        """
         return (King(), Queen(), Bishop(), Knight(), Rook(), Pawn())
 
     @classmethod
     def char_to_piece(cls, char):
+        """
+        Convert a character (string of length one) into either a Piece
+        object or the EMPTY character.  Throws ValueError if the
+        character is not recognized.
+        """
         if char == cls.EMPTY:
             return cls.EMPTY
         for piece in cls.valid_pieces():
@@ -448,29 +484,41 @@ class Board(FieldMixin):
         raise ValueError("Invalid piece: {0}".format(char))
 
     def is_on_board(self, loc):
+        """
+        @return True iff the location is on the board
+        """
         (row, col) = loc
         return row >=0 and row < self.rows and col >= 0 and col < self.cols
 
-    @classmethod
-    def from_string(cls, string, separator='\n'):
-        rows = string.split(separator)
-        pieces = [(cls.char_to_piece(char), (ridx, cidx))
-                  for (ridx, row) in enumerate(rows)
-                  for (cidx, char) in enumerate(row)]
-        pieces = [(piece, loc) for (piece, loc) in pieces if piece != cls.EMPTY]
-        return cls(rows=len(rows),
-                   cols=len(rows[0]),
-                   pieces=pieces)
-
     def add_piece(self, piece, loc):
+        """
+        Add a piece at a location.  Raises AssertionError if the location
+        isn't empty.
+        """
         assert(self.is_empty(loc))
         self.pieces = sorted(self.pieces + [(piece, loc)])
         self._set(loc, piece)
 
-    def move(self, piece, start, end, force=False):
-        if not force:
-            assert(self.get(start) == piece)
-            assert((piece, start) in self.pieces)
+    def move(self, piece, start, end, reverse=False):
+        """
+        Move a piece to a new location.  Raises AssertionError if
+         - the piece isn't currently in the start location
+         - the piece is not allowed to make that move
+
+        If we are moving to a location that is currently occupied,
+        it's a capture and we'll remove the captured piece.
+
+        @param reverse: True if we are moving a piece back (in which
+        case, the movement rules are different)
+
+        """
+        assert(self.get(start) == piece)
+        assert((piece, start) in self.pieces)
+        if reverse:
+            assert(self.is_empty(end))
+            assert((end in piece.moved_from(self, start)) or
+                   (end in piece.attacked_from(self, start)))
+        else:
             if self.is_empty(end):
                 assert(end in piece.moves(self, start))
             else:
@@ -482,10 +530,19 @@ class Board(FieldMixin):
              if loc != start and loc != end] + [(piece, end)])
 
     def capture(self, piece, start, end):
+        """
+        Move a piece to a new location, capturing the piece that was
+        there.
+
+        """
         assert(not self.is_empty(end))
         self.move(piece, start, end)
 
-    def possible_moves(self):
+    def get_capturing_moves(self):
+        """
+        Return all valid capturing moves from the current board
+        configuration
+        """
         for (piece, loc) in self.pieces:
             for end in piece.attacks(self, loc):
                 if not self.is_empty(end):
@@ -495,10 +552,16 @@ def do_search(board):
     """
     Solve a solitaire chess game.  Just does a depth first search on
     all possible moves.
+
+    @return a tuple (solved, moves).  Solved is a boolean which is
+    true if we found a solution.  Moves is a list of the moves to
+    solve the puzzle.  A move is a tuple (piece, start-loc, end-loc)
+    where start-loc and end-loc are locations (tuples of the form
+    (row, col)).
     """
     if len(board.pieces) <= 1:
         return (True, [])
-    for (piece, loc, end) in board.possible_moves():
+    for (piece, loc, end) in board.get_capturing_moves():
         captured = board.get(end)
         board.capture(piece, loc, end)
         logging.debug(board)
@@ -506,7 +569,7 @@ def do_search(board):
         if res:
             moves.append((piece, loc, end))
             return (True, moves)
-        board.move(piece, end, loc, force=True)
+        board.move(piece, end, loc, reverse=True)
         board.add_piece(captured, end)
     return (False, None)
 
@@ -523,8 +586,12 @@ def generate_random_puzzle(board, npieces):
     # Exclude Queens, they make things too easy.
     valid = [v for v in valid if v != Queen()]
     started_with_pieces = len(board.pieces) > 0
+    # This will be the list of moves we made to create the board, so
+    # it will end up being the solution to the puzzle (when reversed)
+    moves = []
     while len(board.pieces) < npieces:
         if len(board.pieces) < 1:
+            # The board is empty, add a new piece
             row = random.randrange(board.rows)
             col = random.randrange(board.cols)
             piece = random.sample(valid, 1)[0]
@@ -532,16 +599,26 @@ def generate_random_puzzle(board, npieces):
             logging.debug(board)
             board.add_piece(piece, (row, col))
         else:
+            # The board is not empty.  To generate a new piece, move
+            # one of the existing pieces backwards and place a new
+            # piece where it was -- a sort of reverse capture.
             for (piece, loc) in random.sample(board.pieces, len(board.pieces)):
+                # Get the list of all the places that this piece could
+                # have come from
                 locs = piece.attacked_from(board, loc)
                 if len(locs) <= 0:
+                    # There are no places that this piece could have
+                    # come from.  In that case, skip this piece and
+                    # try another.
                     continue
                 orig_loc = random.sample(locs, 1)[0]
                 new_piece = random.sample(valid, 1)[0]
                 logging.debug("Moving {0} from {1} to {2}".
                               format(piece, loc, orig_loc))
-                board.move(piece, loc, orig_loc, force=True)
+                board.move(piece, loc, orig_loc, reverse=True)
                 board.add_piece(new_piece, loc)
+                moves.append((piece, orig_loc, loc))
+                logging.debug(board)
                 break
             else:
                 if started_with_pieces:
@@ -550,6 +627,7 @@ def generate_random_puzzle(board, npieces):
                     logging.debug("No valid moves, starting over")
                     board = Board(rows=board.rows, cols=board.cols)
                     started_with_pieces = False
+                    moves = []
                 else:
                     # This could happen randomly.  For example, if the
                     # first piece you place is a pawn in the first
@@ -559,7 +637,7 @@ def generate_random_puzzle(board, npieces):
                     raise ValueError("Unable to place {0} pieces on board".
                                      format(npieces))
 
-    return board
+    return (board, moves)
 
 # ------------------------------------------------------------- #
 
