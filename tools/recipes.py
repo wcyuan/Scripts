cf8cbec229f35f6246c332a2f7aa2a491deee1d5
wcyuan <conanyuan+github@gmail.com>
2015-05-21 08:50:16 -0400

    add memoize

--- tools/recipes.py
+++ tools/recipes.py
@@ -263,3 +263,19 @@ class FP(object):
         return cls.GT(a, b) or cls.EQ(a, b)
 
 # --------------------------------------------------------------------------- #
+
+def memoize(obj):
+  """from https://wiki.python.org/moin/PythonDecoratorLibrary
+  """
+  cache = obj.cache = {}
+
+  @functools.wraps(obj)
+  def memoizer(*args, **kwargs):
+    key = str(args) + str(kwargs)
+    if key not in cache:
+      cache[key] = obj(*args, **kwargs)
+    return cache[key]
+
+  return memoizer
+
+# --------------------------------------------------------------------------- #
