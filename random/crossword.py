a42c39a52e334dc9d956b79eb2334beba6e5eb8a
conan <conanyuan@gmail.com>
2015-06-08 22:12:45 -0400

    Add crossword.py
    crossword.py prints 4x4 grids where every row and column is a valid 4
    letter word

---
+++ random/crossword.py
@@ -0,0 +1,544 @@
+#!/usr/bin/env python
+"""
+Given a list of 4-letter words, create a 4x4 crossword -- a 4x4 grid
+where every row and every column is a valid word. In fact, produce
+every such grid.
+
+With --test, it should print:
+
+--
+a b c
+b c a
+c a b
+--
+c a b
+a b c
+b c a
+--
+b c a
+c a b
+a b c
+
+"""
+
+# --------------------------------------------------------------------------- #
+
+from __future__ import absolute_import, division, with_statement
+
+import contextlib
+import logging
+import math
+import optparse
+import urllib
+
+# --------------------------------------------------------------------------- #
+
+logging.basicConfig(format='[%(asctime)s '
+                    '%(funcName)s:%(lineno)s %(levelname)-5s] '
+                    '%(message)s')
+
+DEFAULT_SIZE = 4
+
+# --------------------------------------------------------------------------- #
+
+def main():
+    (opts, args) = getopts()
+
+    if opts.test:
+        words = ['abc', 'bca', 'cab']
+        opts.start = 0
+    else:
+        words = get_words()
+
+    print_solutions(words, opts.start)
+    
+
+def getopts():
+    parser = optparse.OptionParser()
+    parser.add_option('--start', type=int, default=10000)
+    parser.add_option('--test', action='store_true')
+    parser.add_option('--verbose',  action='store_true')
+    parser.add_option('--log_level',
+                      help="The level to log at")
+    (opts, args) = parser.parse_args()
+
+    if opts.verbose:
+        logat(logging.DEBUG)
+
+    if opts.log_level is not None:
+        logat(opts.log_level)
+
+    return (opts, args)
+
+def logat(level):
+    if isinstance(level, basestring):
+        level = getattr(logging, level.upper())
+    logging.getLogger().setLevel(level)
+    logging.info("Setting log level to %s", level)
+
+# --------------------------------------------------------------------------- #
+
+class Trie(object):
+    """
+    A Trie is a tree-like structure for storing strings, usually,
+    though it can be used to store many things.  The value to be
+    stored usually determines where it should be stored in the tree.
+    Usually, the Trie stores strings.  Each node of the tree has edges
+    corresponding to each letter.  The value is the path through the
+    tree that you take to determine which node stores the value.
+    """
+    def __init__(self, values=None, value_to_path=None):
+        """
+        Create a new Trie.  If we are seeded with some values, insert those.
+        """
+        self.children = {}
+        self.value = None
+        if value_to_path is None:
+            self.value_to_path_func = self._default_value_to_path
+        else:
+            self.value_to_path_func = value_to_path
+        if values is not None:
+            for value in values:
+                self.insert(value)
+
+    @classmethod
+    def _default_value_to_path(cls, value):
+        """
+        Where should we store a particular value (if the user doesn't
+        insert us with a particular path).  We just treat the value as
+        an iterable, if possible.  Otherwise, we convert it to a string.
+        >>> Trie._default_value_to_path('abc')
+        'abc'
+        >>> Trie._default_value_to_path(3)
+        '3'
+        """
+        try:
+            iter(value)
+            return value
+        except TypeError:
+            return str(value)
+
+    def insert(self, value, path=None):
+        """
+        Insert a value.
+        """
+        if path is None:
+            path = self.value_to_path_func(value)
+        if not path:
+            self.value = value
+            return self
+        else:
+            if path[0] not in self.children:
+                self.children[path[0]] = Trie()
+            self.children[path[0]].insert(value, path[1:])
+            return self
+
+    def subtrie(self, path, create=False):
+        """
+        Given a path, return the subtrie rooted at the end of that path.
+        If the subtrie doesn't exist in the trie, return None.  If
+        create is true, then it will create the path if necessary.
+        >>> print str(Trie(['agb', 'agbas', 'basd', 'agbsdfa']).subtrie('agb', create=False))
+        agb
+         a : None
+          s : agbas
+         s : None
+          d : None
+           f : None
+            a : agbsdfa
+        """
+        if not path:
+            return self
+        if path[0] not in self.children:
+            if not create:
+                return None
+            self.children[path[0]] = Trie()
+        return self.children[path[0]].subtrie(path[1:], create=create)
+
+    def setdefault(self, path, default):
+        """
+        Get the value at the end of a particular path.  If there is no
+        value there, set it to the given default
+        >>> t = Trie(['agb', 'agbas', 'basd'])
+        >>> t.setdefault('foo', []).append('bar')
+        >>> t.setdefault('foo', []).append('baz')
+        >>> t.get('foo')
+        ['bar', 'baz']
+        """
+        subtrie = self.subtrie(self.value_to_path_func(path), create=True)
+        if subtrie.value is None:
+            subtrie.value = default
+        return subtrie.value
+
+    def get(self, path, default=None):
+        """
+        Get the value at the end of a particular path.  If there is no
+        value there, return None.
+        >>> t = Trie(['agb', 'agbas', 'basd'])
+        >>> t.get('agb')
+        'agb'
+        >>> t.get('foo')
+        >>> t.get('foo', 'bar')
+        'bar'
+        """
+        subtrie = self.subtrie(self.value_to_path_func(path), create=False)
+        return default if subtrie is None else subtrie.value
+
+    def to_string(self, depth=0):
+        """
+        Return a string representation of a Trie.
+        >>> print str(Trie(['agb', 'agbas', 'basd']))
+        None
+         a : None
+          g : None
+           b : agb
+            a : None
+             s : agbas
+         b : None
+          a : None
+           s : None
+            d : basd
+        """
+        retval = str(self.value)
+        for sub in self.children:
+            retval += '\n{0}{1} : {2}'.format(
+                ' ' * (depth+1), # indent
+                sub,
+                self.children[sub].to_string(depth+1))
+        return retval
+
+    def __iter__(self):
+        """
+        Iterate through the keys of the Trie, sorted.
+        Converts keys into strings and concatenates them.
+        >>> list(Trie(['zasdf', 'agb', 'agbas', 'basd']))
+        ['agb', 'agbas', 'basd', 'zasdf']
+        >>> t = Trie()
+        >>> t.insert(range(3), path='abc')
+        Trie(['abc'])
+        >>> list(t)
+        ['abc']
+        """
+        if self.value is not None:
+            yield ""
+        for sub in sorted(self.children):
+            for key in self.children[sub]:
+                yield str(sub) + str(key)
+
+    def itervalues(self):
+        """
+        Iterate through the values in the Trie, sorted.
+        >>> list(Trie(['zasdf', 'agb', 'agbas', 'basd']).itervalues())
+        ['agb', 'agbas', 'basd', 'zasdf']
+        >>> t = Trie()
+        >>> t.insert(range(3), path='abc')
+        Trie(['abc'])
+        >>> list(t.itervalues())
+        [[0, 1, 2]]
+        """
+        if self.value is not None:
+            yield self.value
+        for sub in sorted(self.children):
+            for value in self.children[sub].itervalues():
+                yield value
+
+    def __str__(self):
+        return self.to_string()
+
+    def __repr__(self):
+        """
+        If the Trie has fewer than 6 elements, print a string that will
+        reconstruct the Trie.  Otherwise, print the default repr.
+        >>> Trie(['agb', 'agbas', 'basd'])
+        Trie(['agb', 'agbas', 'basd'])
+        >>> Trie(range(3))
+        Trie(['0', '1', '2'])
+        >>> Trie(range(9)) # doctest: +ELLIPSIS
+        <solver.Trie object at ...>
+        """
+        itr = iter(self)
+        nvals = tuple(next(itr) for _ in xrange(6))
+        if len(nvals) < 6:
+            return '{cn}({lst})'.format(cn=self.__class__.__name__,
+                                        lst=list(self))
+        else:
+            return super(Trie, self).__repr__()
+
+# ------------------------------------------------------------- #
+
+class FieldMixin(object):
+    """
+    A Mixin that applies to classes that have a few key attributes
+    that determine all the other behavior.  Those attributes have to
+    be settable as keyword arguments in the __init__ function.  In
+    that case, if you put the names of those attributes in _flds, then
+    this Mixin will provide repr, cmp, and hash functions.
+    """
+    @property
+    def _flds(self):
+        raise NotImplementedError
+
+    @staticmethod
+    def _vals(obj):
+        return tuple(getattr(obj, fld) for fld in obj._flds)
+
+    def __repr__(self):
+        """
+        Must satisfy:
+          obj == eval(repr(obj))
+        for any obj
+        """
+        cn = self.__class__.__name__
+        fmt = ('{cn}(' +
+               ',\n{pd} '.join('{0} = {{self.{0}!r}}'.format(fld)
+                               for fld in (self._flds)) +
+               ')')
+        return fmt.format(self=self, cn=cn, pd=' '*len(cn))
+
+    def __cmp__(self, other):
+        tcmp = cmp(type(self), type(other))
+        if tcmp == 0:
+            return cmp(self._vals(self), self._vals(other))
+        else:
+            return tcmp
+
+    def __hash__(self):
+        return hash(self._vals(self))
+
+
+class Board(FieldMixin):
+    """
+    >>> b = Board(rows=4, cols=4, letters={(1, 2): 'A', (1, 1): 'B'})
+    >>> b == eval(repr(b))
+    True
+    >>> b
+    Board(rows = 4,
+          cols = 4,
+          letters = {(1, 2): 'A', (1, 1): 'B'})
+    >>> print b
+    --
+    . . . .
+    . B A .
+    . . . .
+    . . . .
+    >>> b.get_range((1, 1), (1, 2))
+    ['B', 'A']
+    """
+    EMPTY = '.'
+    BLOCK = '#'
+
+    def __init__(self, rows=DEFAULT_SIZE, cols=DEFAULT_SIZE, letters=None):
+        """
+        A board is represented as a list of rows x cols values.  Each
+        value can either be empty, or it can be blacked out, or it can
+        contain a letter.
+        A spot on the board is usually referred to by a "location" or
+        "loc", which is a (row, col) tuple.
+        """
+        super(Board, self).__init__()
+        self.rows = rows
+        self.cols = cols
+        self.board = []
+        self.letters = letters if letters else {}
+        self._reset_board()
+
+    def __str__(self):
+        return ('--\n' +
+                '\n'.join(' '.join(self._get((r, c)) for c in xrange(self.cols))
+                          for r in xrange(self.rows)))
+
+    @property
+    def _flds(self):
+        return ('rows', 'cols', 'letters')
+
+    def _loc_to_idx(self, loc):
+        """
+        Translates from a location (a (row, col) tuple) to an index into
+        the board, which is just a list of values.
+        """
+        (row, col) = loc
+        return row * self.cols + col
+
+    def _get(self, loc):
+        idx = self._loc_to_idx(loc)
+        try:
+            return self.board[idx]
+        except IndexError:
+            (tp, value, traceback) = sys.exc_info()
+            value = 'Index {0}, from {1} out of range: {2}'.format(idx, loc, value)
+            raise tp, value, traceback
+
+    def _set(self, loc, value):
+        idx = self._loc_to_idx(loc)
+        try:
+            self.board[idx] = value
+        except IndexError:
+            (tp, value, traceback) = sys.exc_info()
+            value = 'Index {0} out of range: {1}'.format(idx, value)
+            raise tp, value, traceback
+        return self
+
+    def _reset_board(self):
+        """
+        Call this after changing self.queens to recompute which locations
+        are being attacked.
+        """
+        self.board = [self.EMPTY] * (self.rows * self.cols)
+        for loc in self.letters:
+            self.add_letter(loc, self.letters[loc])
+        return self
+
+    def add_letter(self, loc, letter):
+        """
+        Add a letter to the board at the given location.
+        """
+        if letter in (self.EMPTY, self.BLOCK):
+            raise ValueError("Invalid letter: {0}".format(letter))
+        if loc not in self.letters:
+            self.letters[loc] = letter
+        self._set(loc, letter)
+        return self
+
+    def clear_loc(self, loc):
+        """
+        Remove any letter from the given location.
+        """
+        if self._get(loc) != self.BLOCK:
+            self._set(loc, self.EMPTY)
+        if loc in self.letters:
+            del self.letters[loc]
+            self._reset_board()
+        return self
+
+    def _is_empty(self, loc):
+        return self._get(loc) == self.EMPTY
+
+    def _all_locs(self):
+        return ((row, col)
+                for row in xrange(self.rows)
+                for col in xrange(self.cols))
+
+    def empty_locs(self):
+        return (loc for loc in self._all_locs()
+                if self._is_empty(loc))
+
+    def get(self, loc):
+        return self._get(loc)
+
+    def get_range(self, start_loc, end_loc, incr=None):
+        """
+        This is a bit hacky, it doesn't handle corner cases well, 
+        such as if the end_loc is not on the board or is either above
+        or to the left of the start_loc.  Use with caution.  
+        """
+        if not incr:
+            (srow, scol) = start_loc
+            (erow, ecol) = end_loc
+            diff = (erow - srow, ecol - scol)
+            incr = [int(math.copysign(1, erow-srow)),
+                    int(math.copysign(1, ecol-scol))]
+            if diff[0] == 0:
+                incr[0] = 0
+            elif diff[1] == 0:
+                incr[1] = 0
+            elif diff[0] != diff[1]:
+                raise ValueError(
+                    "Can't determine increment from {0} to {1}".format(
+                        start_loc, end_loc))
+        loc = start_loc
+        while loc[0] >=0 and loc[0] < self.rows and loc[1] >= 0 and loc[1] < self.cols:
+            yield self._get(loc)
+            if loc == end_loc:
+                break
+            loc = (loc[0] + incr[0], loc[1] + incr[1])
+
+
+# --------------------------------------------------------------------------- #
+
+def next_empty(board):
+    """
+    It would probably be more efficient if we did this in a different order,
+    alternating between rows and columns.
+    """
+    try:
+        return next(board.empty_locs())
+    except StopIteration:
+        return None
+
+def by_prefix(trie, prefixes):
+    """
+    Given a list of prefixes and lengths, find all words
+    of the given length that start with the given prefix,
+    and return all valid next letters
+    """
+    letters = set()
+    for prefix in prefixes:
+        subtrie = trie.subtrie(prefix)
+        if not subtrie:
+            print "No possibilities for prefix: ", prefix
+            #raise ValueError("No possibilities for prefix: {0}".format(prefix))
+            continue
+        next_letters = subtrie.children.keys()
+        if letters:
+            letters.intersection_update(next_letters)
+        else:
+            letters.update(next_letters)
+    return letters
+
+def get_possible_letters(trie, board, loc):
+    (row, col) = loc
+    row_pfx = ''.join(board.get((row, c)) for c in xrange(0, col))
+    col_pfx = ''.join(board.get((r, col)) for r in xrange(0, row))
+    return by_prefix(trie, [row_pfx, col_pfx])
+
+def helper(trie, board):
+    # Nothing stops us from using the same word in multiple
+    # places on the board, such as:
+    #   a a h s
+    #   a d i t
+    #   h i v e
+    #   s t e p
+    spot = next_empty(board)
+    if not spot:
+        yield board
+        return
+    letters = get_possible_letters(trie, board, spot)
+    for letter in letters:
+        board.add_letter(spot, letter)
+        for solution in helper(trie, board):
+            yield solution
+        board.clear_loc(spot)
+
+def solve(words, nchars=None):
+    if not nchars:
+        nchars = len(words[0])
+    t = Trie(w.lower() for w in words)
+    b = Board(rows=nchars, cols=nchars)
+    
+    for solution in helper(t, b):
+        yield solution
+
+def get_words():
+    url = 'https://raw.githubusercontent.com/wcyuan/Scripts/26b963ce9746f5dde16855dece9f2a1da2206f00/random/4_letter_enable2k_words.txt'
+    with contextlib.closing(urllib.urlopen(url)) as fd:
+        words = [word.rstrip() for word in fd.readlines()
+                 if not word.startswith("#")]
+    return words
+
+def print_solutions(words, start=300, end=None):
+    if not end:
+        end = start + 100
+    nvalues = 0
+    for board in solve(words):
+        nvalues += 1
+        if nvalues >= start:
+            print board
+        if nvalues >= end:
+            break
+
+# --------------------------------------------------------------------------- #
+
+if __name__ == "__main__":
+    main()
+
+# --------------------------------------------------------------------------- #
