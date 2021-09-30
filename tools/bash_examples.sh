791bc778560c99aa39152d39a02877610b2da07b
wcyuan <wcyuan@users.noreply.github.com>
2021-09-29 22:40:28 -0400

    save_output_to_log: take filename as an argument

--- tools/bash_examples.sh
+++ tools/bash_examples.sh
@@ -14,12 +14,12 @@ function run() {
 
 # https://unix.stackexchange.com/questions/145651/using-exec-and-tee-to-redirect-logs-to-stdout-and-a-log-file-in-the-same-time
 function save_output_to_log() {
-  readonly LOG_FILE="run_pipeline.$(date +%Y%m%d-%H%M%S).log"
+  readonly LOG_FILE="$1"
   touch "$LOG_FILE"
   exec &> >(tee -a "$LOG_FILE")
   echo "Saving all output to $LOG_FILE"
 }
-save_output_to_log
+save_output_to_log "log_file.$(date +%Y%m%d-%H%M%S).log"
 
 echo stdout
 echo stderr >&2
